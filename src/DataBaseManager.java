c807a3f137ecac2e948de6ab538ce223fed0619c
Aniket Mahajan <mahajananiket207@gmail.com>
2025-03-15 10:38:33 +0530

    Project init

---
+++ src/DataBaseManager.java
@@ -0,0 +1,7 @@
+public class DataBaseManager {
+    private static final String url = "jdbc:mysql://localhost:3306/mydb";
+    private static final String user = "root";
+    private static final String password = "AM@123aniket";
+
+
+}
