e693470ee8564e997251fde31e42605c30035dd7
Aniket Mahajan <mahajananiket207@gmail.com>
2025-03-15 10:55:32 +0530

    create method to add user in database

--- src/DataBaseManager.java
+++ src/DataBaseManager.java
@@ -1,7 +1,24 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DataBaseManager {
-    private static final String url = "jdbc:mysql://localhost:3306/mydb";
-    private static final String user = "root";
-    private static final String password = "AM@123aniket";
+//    private static final String url = "jdbc:mysql://localhost:3306/mydb";
+//    private static final String user = "root";
+//    private static final String password = "AM@123aniket";
 
 
+    public void insetToDB(PreparedStatement pstmt, int id, String username, String email, String password) throws SQLException {
+        try{
+            pstmt.setInt(1, id);
+            pstmt.setString(2, username);
+            pstmt.setString(3, email);
+            pstmt.setString(4, password);
+            pstmt.executeUpdate();
+            System.out.println("User added successfully!");
+
+        } catch (SQLException e) {
+            System.out.println("Something went wrong" + e);
+            e.printStackTrace();
+        }
+    }
 }
